011e8a54b2d1a6063d3c94719d3ab27a8f227863
Bryce Slade <bryceslade@gmail.com>
2022-01-25 13:37:53 -0700

    basic layout structured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import MovieCard from './components/MovieCard';
+import './app.css';
 
 function App() {
+
+  const API_URL = 'https://api.themoviedb.org/3'
+  const [movies, setMovies] = useState([])
+  const [searchKey, setSearchKey] = useState('')
+  const [selectedMovie, setSelectedMovie] = useState({})
+
+  const getMovies = (searchKey) => {
+    const type = searchKey ? 'search' : 'discover'
+    axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: '0ef14fb15ebf8738272817a78a6fe9e1',
+        query: searchKey
+      }
+    })
+    .then(res => {
+      setMovies(res.data.results)
+      setSelectedMovie(res.data.results[0])
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
+  const renderMovies = () => (
+    movies.map(movie => (<MovieCard key={movie.id} movie={movie}/>))
+  )
+
+  const searchMovies = (e) => {
+    e.preventDefault()
+    getMovies(searchKey)
+  }
+  
+
   return (
     <div className="App">
-      <h1>test</h1>
+      <header className='header'>
+        <div className='header-content max-center'>
+          <span>Movie Trailer App</span>
+          <form onSubmit={searchMovies}>
+            <input 
+              type='text' 
+              onChange={(e) => setSearchKey(e.target.value)}
+            />
+            <button type='submit'>Search!</button>
+          </form>
+        </div>
+      </header>
+        <div className='hero'>
+          <div className='hero-content'>
+            <h1>{selectedMovie.title}</h1>
+          </div>
+        </div>
+        <div className='container max-center'>
+          {renderMovies()}
+        </div>
     </div>
   );
 }
